7de5a6e628cfe90fa6641135cb74f7faddaefe8a
Rishik <rishik3555@gmail.com>
2025-07-10 14:48:37 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MonacoConfigLoader from '@/components/MonacoConfigLoader';
+import MonacoConfigLoader from "@/components/MonacoConfigLoader";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <MonacoConfigLoader />
         {children}
+        <Analytics />
       </body>
     </html>
   );
